8cd8b7b6ea43e4230d8c40fd7291fdf0ab5a5335
oetproduction <production@oceanexplorationtrust.org>
2024-01-23 10:58:52 -0500

    Update generate_flightlog_wca.py

--- generate_flightlog_wca.py
+++ generate_flightlog_wca.py
@@ -3,14 +3,14 @@ import csv
 from datetime import datetime
 from PIL import Image  # Add PIL library for image format checking
 
-# Prompt the user for the path of the TSV file
-tsv_folder = input("Enter the path of the folder containing the TSV file: ")
-tsv_filename = input("Enter the name of the TSV file: ")
+# Prompt the user for the folder containing the TSV file
+tsv_folder = input("Enter the folder containing the TSV file: ")
 
-# Prompt the user for the folder where the flight log should be exported
-output_folder = input("Enter the folder where the flight log should be exported: ")
+# List all common image formats to accept
+COMMON_IMAGE_EXTENSIONS = {".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff"}
 
 # Configuration
+TSV_FILENAME = "H2021.NAV3D.M1.sampled.tsv"
 TIMESTAMP_FORMAT = "%Y%m%dT%H%M%S"
 REFERENCE_DEPTH = 0
 
@@ -47,11 +47,7 @@ def read_image_filenames(image_folder):
 
 # Function to check if a file is an image
 def is_image_file(filename):
-    try:
-        Image.open(os.path.join(image_folder, filename))
-        return True
-    except Exception as e:
-        return False
+    return any(filename.lower().endswith(ext) for ext in COMMON_IMAGE_EXTENSIONS)
 
 # Function to estimate location
 def estimate_location(image_data, data_rows):
@@ -69,4 +65,23 @@ def generate_flight_log(image_data, output_filename):
         for image in image_data:
             line = "{};{};{};{}".format(image["FILENAME"], image["LAT_EST"], image["LONG_EST"], image["ALTITUDE_EST"])
             if line not in unique_locations:
-        
+                f.write(line + "\n")
+                unique_locations.add(line)
+
+if __name__ == "__main__":
+    tsv_filename = os.path.join(tsv_folder, TSV_FILENAME)
+    data_rows = read_tsv_data(tsv_filename)
+
+    image_folder = tsv_folder
+    image_data = read_image_filenames(image_folder)
+
+    # Prompt the user for the folder where the flight log should be exported
+    output_folder = input("Enter the folder where the flight log should be exported: ")
+    flight_log_filename = os.path.join(output_folder, "flight_log.txt")
+
+    estimate_location(image_data, data_rows)
+    generate_flight_log(image_data, flight_log_filename)
+
+    # Print summary
+    print("Files examined: {}".format(len(image_data)))
+    print("Data rows interpreted: {}".format(len(data_rows)))
